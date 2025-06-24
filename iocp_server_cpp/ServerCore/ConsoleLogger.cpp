b2c3fe4233f127297988690d423c8ed9e0c87ed7
Jinwoo Jung <wlsdn2749@gmail.com>
2025-06-25 01:02:51 +0900

    fix: In Will be added ConsoleLogger's Output, Korean output has been broken out. fixed it

--- iocp_server_cpp/ServerCore/ConsoleLogger.cpp
+++ iocp_server_cpp/ServerCore/ConsoleLogger.cpp
@@ -1,10 +1,12 @@
 #include "pch.h"
 #include "ConsoleLogger.h"
+#include <Windows.h>
+#include <cwchar>
 
 ConsoleLogger::ConsoleLogger()
 {
-	_stdOut = ::GetStdHandle(STD_OUTPUT_HANDLE);
-	_stdErr = ::GetStdHandle(STD_ERROR_HANDLE);
+    _stdOut = ::GetStdHandle(STD_OUTPUT_HANDLE);
+    _stdErr = ::GetStdHandle(STD_ERROR_HANDLE);
 }
 
 ConsoleLogger::~ConsoleLogger()
@@ -13,52 +15,49 @@ ConsoleLogger::~ConsoleLogger()
 
 void ConsoleLogger::WriteStdOut(Color color, const WCHAR* format, ...)
 {
-	if (format == nullptr)
-		return;
+    if (!format) return;
+    SetColor(true, color);
 
-	SetColor(true, color);
+    WCHAR buffer[BUFFER_SIZE];
+    va_list ap;
+    va_start(ap, format);
+    vswprintf_s(buffer, BUFFER_SIZE, format, ap);
+    va_end(ap);
 
-	va_list ap;
-	va_start(ap, format);
-	::vwprintf(format, ap);
-	va_end(ap);
+    DWORD written = 0;
+    WriteConsoleW(_stdOut, buffer, static_cast<DWORD>(wcslen(buffer)), &written, nullptr);
 
-	fflush(stdout);
-
-	SetColor(true, Color::WHITE);
+    SetColor(true, Color::WHITE);
 }
 
 void ConsoleLogger::WriteStdErr(Color color, const WCHAR* format, ...)
 {
-	WCHAR buffer[BUFFER_SIZE];
+    if (!format) return;
+    SetColor(false, color);
 
-	if (format == nullptr)
-		return;
+    WCHAR buffer[BUFFER_SIZE];
+    va_list ap;
+    va_start(ap, format);
+    vswprintf_s(buffer, BUFFER_SIZE, format, ap);
+    va_end(ap);
 
-	SetColor(false, color);
+    DWORD written = 0;
+    WriteConsoleW(_stdErr, buffer, static_cast<DWORD>(wcslen(buffer)), &written, nullptr);
 
-	va_list ap;
-	va_start(ap, format);
-	::vswprintf_s(buffer, BUFFER_SIZE, format, ap);
-	va_end(ap);
-
-	::fwprintf_s(stderr, buffer);
-	fflush(stderr);
-
-	SetColor(false, Color::WHITE);
+    SetColor(false, Color::WHITE);
 }
 
 void ConsoleLogger::SetColor(bool stdOut, Color color)
 {
-	static WORD SColors[]
-	{
-		0,
-		FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE,
-		FOREGROUND_RED | FOREGROUND_INTENSITY,
-		FOREGROUND_GREEN | FOREGROUND_INTENSITY,
-		FOREGROUND_BLUE | FOREGROUND_INTENSITY,
-		FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY
-	};
+    static WORD SColors[] = {
+        0,
+        FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE,
+        FOREGROUND_RED | FOREGROUND_INTENSITY,
+        FOREGROUND_GREEN | FOREGROUND_INTENSITY,
+        FOREGROUND_BLUE | FOREGROUND_INTENSITY,
+        FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY
+    };
 
-	::SetConsoleTextAttribute(stdOut ? _stdOut : _stdErr, SColors[static_cast<int32>(color)]);
+    ::SetConsoleTextAttribute(stdOut ? _stdOut : _stdErr,
+        SColors[static_cast<int32>(color)]);
 }
